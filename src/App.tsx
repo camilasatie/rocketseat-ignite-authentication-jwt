57e6c889bf8ec406e6f933b6711e323d5ba3c957
Camila Satie <camilasatie.dev@gmail.com>
2021-05-16 17:25:33 -0300

    add login component with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import "./styles.css";
+import { AuthProvider } from "../contexts/AuthContent";
+import { Login } from "./components/Login";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <AuthProvider>
+      <Login />
+    </AuthProvider>
   );
 }
---
+++ src/components/Login.tsx
@@ -0,0 +1,36 @@
+import { FormEvent, useContext, useState } from "react";
+import { AuthContext } from "../../contexts/AuthContent";
+
+import "./styles.css";
+
+export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useContext(AuthContext);
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const data = {
+      email,
+      password
+    };
+
+    await signIn(data);
+  }
+  return (
+    <form onSubmit={handleSubmit} className="container">
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Enviar</button>
+    </form>
+  );
+}
